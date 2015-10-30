4c017b41bc301aaff09133ab54dd17ce4824df34
Otavio Carneiro <otavio@carneiro.blog.br>
2015-10-30 16:05:35 -0200

    Primeira versao Serial

---
+++ mearm_serial/mearm_serial.ino
@@ -0,0 +1,63 @@
+// Controle de um braco robotico meArm usando poteciometros
+// Baseado no exemplo servo -> knob
+
+#include <Servo.h> 
+ 
+Servo servo1;
+Servo servo2;
+Servo servo3;
+ 
+int controle1 = A0;
+int controle2 = A1;
+int controle3 = A2;
+ 
+String comandoLido = "";
+boolean comandoCompleto = false;
+
+void setup() 
+{ 
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  
+  Serial.begin(9600);
+  comandoLido.reserve(20);
+
+} 
+ 
+void loop() 
+{ 
+  if (comandoCompleto) {
+    int valorComando = comandoLido.toInt();
+    Serial.println(valorComando); 
+    comandoLido = "";
+    comandoCompleto = false;
+  }
+  
+  lePotAcionaServo(servo1, controle1);
+  lePotAcionaServo(servo2, controle2);
+  lePotAcionaServo(servo3, controle3);
+  delay(15);
+} 
+
+int leComando(int controle) {
+  int val = analogRead(controle);
+  val = map(val, 0, 1023, 0, 179);
+  return val;
+}
+
+void lePotAcionaServo(Servo servo, int controle) {
+  int val = leComando(controle);
+  servo.write(val);
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read(); 
+    comandoLido += inChar;
+    if (inChar == '\n') {
+      comandoCompleto = true;
+    } 
+  }
+}
+
